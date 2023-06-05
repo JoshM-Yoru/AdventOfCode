911653c22dae16342432ef85357ff025c188ce93
JoshM-Yoru <jmaciel335i@gmail.com>
2023-06-05 00:06:31 -0400

    completed part 2, refactored part 1 to be able to use the same player variables

--- 2022-cpp/day2/day2.cpp
+++ 2022-cpp/day2/day2.cpp
@@ -22,25 +22,17 @@ int main() {
     string line;
 
     while (getline(infile, line)) {
-        p1 = line[0] - 'A' + 1;
-        p2 = line[2] - 'X' + 1;
+        p1 = line[0] - 'A';
+        p2 = line[2] - 'X';
 
         // Part 1
-        score += p2;
-
-        if (p2 == 1 && p1 == 3) {
-            score += 6;
-        } else if (p2 == 2  && p1 == 1) {
-            score += 6;
-        } else if (p2 == 3 && p1 == 2) {
-            score += 6;
-        } else if (p1 == p2) {
-            score += 3;
-        }
+        score += p2 + 1 + ((p2 - p1 + 4) % 3) * 3;
 
         // Part 2
+        score2 += p2 * 3 + (p1 + p2 + 2) % 3 + 1;
     }
 
     cout << "Day 2" << endl;
     cout << "The total score is: " << score << endl;
+    cout << "The total correct score is: " << score2 << endl;
 }
