16ecd961db97707c5797c5bc3015c7289311d28d
JoshM-Yoru <jmaciel335i@gmail.com>
2023-12-01 14:54:19 -0500

    completed part 2 in Go

--- 2023/Go/day1/main.go
+++ 2023/Go/day1/main.go
@@ -5,107 +5,123 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
 func main() {
-    file, err := os.Open("../../inputs/day1.txt")
-    if err != nil {
-        fmt.Println("Error opening file")
-        os.Exit(0)
-    }
+	file, err := os.Open("../../inputs/day1.txt")
+	if err != nil {
+		fmt.Println("Error opening file")
+		os.Exit(0)
+	}
 
-    defer file.Close()
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-    var lines []string
+	var lines []string
 
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
 
-    if err := scanner.Err(); err != nil {
-        fmt.Println("Error reading line")
-    }
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading line")
+	}
 
-    partOne(&lines)
-    partTwo(&lines)
+	partOne(&lines)
+	partTwo(&lines)
 }
 
 func partOne(file *[]string) {
-    lines := *file
-    sum := 0
+	lines := *file
+	sum := 0
 
-    for i := 0; i < len(lines); i++ {
-        var firstNumber string
-        var lastNumber string
-        foundFirstNumber := false
-        foundAnotherNumber := false
+	for i := 0; i < len(lines); i++ {
+		var firstNumber string
+		var lastNumber string
+		foundFirstNumber := false
+		foundAnotherNumber := false
 
-        for j := 0; j < len(lines[i]); j++ {
-            character := lines[i][j]
-            if unicode.IsDigit(rune(character)) && !foundFirstNumber {
-                firstNumber = string(character)
-                foundFirstNumber = true
-            } else if unicode.IsDigit(rune(character)) && foundFirstNumber {
-                lastNumber = string(character)
-                foundAnotherNumber = true
-            }
-        }
+		for j := 0; j < len(lines[i]); j++ {
+			character := lines[i][j]
+			if unicode.IsDigit(rune(character)) && !foundFirstNumber {
+				firstNumber = string(character)
+				foundFirstNumber = true
+			} else if unicode.IsDigit(rune(character)) && foundFirstNumber {
+				lastNumber = string(character)
+				foundAnotherNumber = true
+			}
+		}
 
-        if !foundAnotherNumber {
-            lastNumber = firstNumber
-        }
+		if !foundAnotherNumber {
+			lastNumber = firstNumber
+		}
 
-        value, err := strconv.Atoi(firstNumber + lastNumber)
-        if err != nil {
-            fmt.Println("error with value")
-            fmt.Println(firstNumber)
-            fmt.Println(lastNumber)
-        }
+		value, err := strconv.Atoi(firstNumber + lastNumber)
+		if err != nil {
+			fmt.Println("error with value")
+			fmt.Println(firstNumber)
+			fmt.Println(lastNumber)
+		}
 
-        sum += value
-    }
+		sum += value
+	}
 
-    fmt.Println(sum)
+	fmt.Println(sum)
 }
 
 func partTwo(file *[]string) {
-    lines := *file
-    sum := 0
+	lines := *file
+	sum := 0
 
-    for i := 0; i < len(lines); i++ {
-        var firstNumber string
-        var lastNumber string
-        foundFirstNumber := false
-        foundAnotherNumber := false
+	table := make(map[string]string)
 
-        for j := 0; j < len(lines[i]); j++ {
-            character := lines[i][j]
-            if unicode.IsDigit(rune(character)) && !foundFirstNumber {
-                firstNumber = string(character)
-                foundFirstNumber = true
-            } else if unicode.IsDigit(rune(character)) && foundFirstNumber {
-                lastNumber = string(character)
-                foundAnotherNumber = true
-            } 
-        }
+	table["one"] = "o1e"
+	table["two"] = "t2o"
+	table["three"] = "th3ee"
+	table["four"] = "f4ur"
+	table["five"] = "f5ve"
+	table["six"] = "s6x"
+	table["seven"] = "se7en"
+	table["eight"] = "ei8ht"
+	table["nine"] = "n9ne"
 
-        if !foundAnotherNumber {
-            lastNumber = firstNumber
-        }
+	for i := 0; i < len(lines); i++ {
+		var firstNumber string
+		var lastNumber string
+		foundFirstNumber := false
+		foundAnotherNumber := false
+		line := lines[i]
+		for k, v := range table {
+            line = strings.ReplaceAll(line, k, v)
+		}
 
-        value, err := strconv.Atoi(firstNumber + lastNumber)
-        if err != nil {
-            fmt.Println("error with value")
-            fmt.Println(firstNumber)
-            fmt.Println(lastNumber)
-        }
+		for j := 0; j < len(line); j++ {
+			character := line[j]
+			if unicode.IsDigit(rune(character)) && !foundFirstNumber {
+				firstNumber = string(character)
+				foundFirstNumber = true
+			} else if unicode.IsDigit(rune(character)) && foundFirstNumber {
+				lastNumber = string(character)
+				foundAnotherNumber = true
+			}
+		}
 
-        sum += value
-    }
+		if !foundAnotherNumber {
+			lastNumber = firstNumber
+		}
 
-    fmt.Println(sum)
+		value, err := strconv.Atoi(firstNumber + lastNumber)
+		if err != nil {
+			fmt.Println("error with value")
+			fmt.Println(firstNumber)
+			fmt.Println(lastNumber)
+		}
+
+		sum += value
+	}
+
+	fmt.Println(sum)
 }
-
