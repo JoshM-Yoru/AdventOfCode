5022009a33423ee4ef4073a8629a8d7d29cbcaa4
JoshM-Yoru <jmaciel335i@gmail.com>
2023-12-04 01:09:07 -0500

    added some logic to day4, need to debug

--- 2023/Go/day4/main.go
+++ 2023/Go/day4/main.go
@@ -4,49 +4,80 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"unicode"
 )
 
-func main()  {
+func main() {
 
-    file, err := os.Open("../../inputs/day4.txt")
-    if err != nil {
-        fmt.Println("Error opening file")
-        os.Exit(0)
-    }
+	file, err := os.Open("../../inputs/day4.txt")
+	if err != nil {
+		fmt.Println("Error opening file")
+		os.Exit(0)
+	}
 
-    defer file.Close()
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-    var contents []string
+	var contents []string
 
-    for scanner.Scan() {
-        contents = append(contents, scanner.Text())
-    }
+	for scanner.Scan() {
+		contents = append(contents, scanner.Text())
+	}
 
-    if err := scanner.Err(); err != nil {
-        fmt.Println("Error reading line")
-    }
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading line")
+	}
 
-    fmt.Println("Part One: ", cardCheck(&contents))
-    
+	fmt.Println("Part One: ", cardCheck(&contents))
+
 }
 
 func cardCheck(contents *[]string) int {
-    lines := *contents
-    sum := 0
+	lines := *contents
+	sum := 0
 
-    for i := 0; i < len(lines); i++ {
-        line := lines[i]
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
 
-        scoreTable := make(map[string]int)
+		scoreTable := make(map[string]int)
+		checkMyCard := false
+		numString := ""
 
-        for j := 10; j < len(line); j++ {
-            fmt.Print(string(line[j]))
-        }
+		for j := 10; j < len(line); j++ {
+			character := line[j]
+			if character == 124 {
+				checkMyCard = true
+			}
+			if unicode.IsDigit(rune(character)) {
+				numString += string(character)
+			} else if len(numString) > 0 && !unicode.IsDigit(rune(character)) && !checkMyCard {
+				scoreTable[numString] = 0
+			}
 
-        fmt.Println()
-    }
+			if checkMyCard {
+				if val, ok := scoreTable[numString]; ok {
+					val++
+				}
+			}
+		}
+			if len(numString) > 0 && checkMyCard {
+				if val, ok := scoreTable[numString]; ok {
+					val++
+				}
+			}
 
-    return sum
+		matchPoints := 0
+		for _, v := range scoreTable {
+			if matchPoints > 2 {
+				matchPoints *= 2
+			} else if v >= 1 {
+				matchPoints += v
+			}
+		}
+
+        sum += matchPoints
+	}
+
+	return sum
 }
