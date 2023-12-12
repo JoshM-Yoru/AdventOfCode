9f8f0804bfdf796e8fae5cf5bf2acac140c40c3c
JoshM-Yoru <jmaciel335i@gmail.com>
2023-12-12 00:51:17 -0500

    started day 5, need to complete sorting algo for when hands are equal

---
+++ 2023/Go/day7/go.mod
@@ -0,0 +1,3 @@
+module aoc.com/day7
+
+go 1.21.5
---
+++ 2023/Go/day7/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Hand struct {
+	Cards  string
+	Points int
+    Strength int
+}
+
+var CardStrength map[string]int
+
+func main() {
+
+	file, err := os.ReadFile("../../inputs/day7.txt")
+	if err != nil {
+		fmt.Println("Error reading file")
+		os.Exit(0)
+	}
+
+	contents := string(file)
+
+	contentsArr := strings.Split(strings.TrimSpace(contents), "\n")
+
+	CardStrength = make(map[string]int)
+
+	CardStrength["A"] = 14
+	CardStrength["K"] = 13
+	CardStrength["Q"] = 12
+	CardStrength["J"] = 11
+	CardStrength["T"] = 10
+	CardStrength["9"] = 9
+	CardStrength["8"] = 8
+	CardStrength["7"] = 7
+	CardStrength["6"] = 6
+	CardStrength["5"] = 5
+	CardStrength["4"] = 4
+	CardStrength["3"] = 3
+	CardStrength["2"] = 2
+
+	hands := []Hand{}
+
+	for _, line := range contentsArr {
+
+		parsedLine := strings.Split(strings.TrimSpace(line), " ")
+
+		points, err := strconv.Atoi(parsedLine[1])
+		if err != nil {
+			fmt.Println("Not a number: ", parsedLine[1])
+		}
+
+		hands = append(hands, Hand{
+			Cards:  parsedLine[0],
+			Points: points,
+            Strength: handParser(&parsedLine[0]),
+		})
+	}
+
+    newArr := []Hand{}
+    for _, v := range hands {
+        newArr = handSort(newArr, v)
+    }
+
+    fmt.Println(newArr)
+
+}
+
+func handParser(hand *string) int {
+
+	mappedHand := make(map[string]int)
+
+	for _, card := range *hand {
+		mappedHand[string(card)]++
+	}
+
+	if len(mappedHand) == 1 {
+		return 6
+	}
+
+	if len(mappedHand) == 2 {
+		for _, v := range mappedHand {
+			if v == 1 || v == 4 {
+				return 5
+			} else {
+				return 4
+			}
+		}
+	}
+
+	if len(mappedHand) == 3 {
+		for _, v := range mappedHand {
+			if v == 3 {
+				return 3
+			}
+		}
+        return 2
+	}
+
+	if len(mappedHand) == 4 {
+		return 1
+	}
+
+
+	return 0
+}
+
+func handSort(hands []Hand, newHand Hand) []Hand {
+    left := 0
+    right := len(hands) - 1
+
+    for left <= right {
+        mid := (left + right) / 2
+        if hands[mid].Strength < newHand.Strength {
+            left = mid + 1
+        } else if hands[mid].Strength == newHand.Strength {
+
+        } else {
+            right = mid - 1
+        }
+    }
+
+    hands = append(hands[:left], append([]Hand{newHand}, hands[left:]...)...)
+    copy(hands[left+1:], hands[left:])
+
+    return hands
+}
